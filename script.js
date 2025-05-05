4ad303863bb3bbcbc9cc9e0c46d6d5f494f8a4fd
nastya <naitova58@gmail.com>
2025-05-05 20:58:26 +0300

    переделка

--- script.js
+++ script.js
@@ -7,3 +7,33 @@ window.addEventListener('scroll', function() {
     heroBackground.style.transform = 'translateY(' + scrollPosition * 0.5 + 'px) translateZ(0)';
 });
 
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const readMoreAboutButton = document.getElementById('read-more-about-button');
+    
+    const hiddenAboutText = document.getElementById('hidden-about-text');
+
+    
+    if (readMoreAboutButton && hiddenAboutText) {
+        
+        readMoreAboutButton.addEventListener('click', function() {
+            
+            if (hiddenAboutText.style.display === 'none' || hiddenAboutText.style.display === '') {
+                
+                hiddenAboutText.style.display = 'block'; 
+                
+                readMoreAboutButton.textContent = 'Свернуть';
+            } else {
+                
+                hiddenAboutText.style.display = 'none';
+               
+                readMoreAboutButton.textContent = 'Подробнее';
+            }
+        });
+    } else {
+        
+        console.error("Кнопка 'Подробнее' или скрытый текст для раздела 'О музее' не найдены.");
+    }
+});
